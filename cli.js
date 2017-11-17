d0c3d21ead56d55d88b6792790d5de9ca3b24e53
WeiChiaChang <ioh04019@gmail.com>
2017-11-17 10:00:48 +0800

    🚨 Test: undergoing

--- cli.js
+++ cli.js
@@ -3,11 +3,17 @@ const ora = require('ora');
 const inquirer = require('inquirer');
 const chalk = require('chalk');
 const Table = require('cli-table2');
+const options = {
+  userAgent: 'Wappalyzer',
+  maxWait: 3000,
+  debug: false
+};
+const wappalyzer = require('wappalyzer')(options);
 
 CFonts.say('Stack-cli', {
 	font: 'block',           //define the font face
 	align: 'center',         //define text alignment
-	colors: ['candy'],        //define all colors
+	colors: ['cyan'],        //define all colors
 	background: 'Black',     //define the background color
 	letterSpacing: 1,        //define letter spacing
 	lineHeight: 1,           //define the line height
@@ -15,22 +21,37 @@ CFonts.say('Stack-cli', {
 	maxLength: '0'           //define how many character can be on one line
 });
 
-// inquirer quesitions prompt
+// inquirer questions prompt from user input
 
 let questions = [
   {
     type: 'input',
     name: 'website',
-    message: 'Which website stack do you wanna see ? (e.g. https://github.com)'
+    message: 'Which website stack do you wanna browse ? (e.g. https://github.com)'
   }
 ];
 
 inquirer.prompt(questions).then(function (answers) {
-  console.log(JSON.stringify(answers, null, '  '));
+  // console.log(JSON.stringify(answers, null, '  '));
+  // console.log(answers.website);
+
+  // spinner start
+  const spinner = ora('Loading data ....').start();
+
+  // should be dynamic by user prompt
+  // wappalyzer.analyze(answers.website)
+  wappalyzer.analyze('https://github.com')
+  .then(json => {
+    spinner.stop();
+    console.log(JSON.stringify(json, null, 2));
+    console.log(table.toString());
+  })
+  .catch(error => {
+    console.error(error);
+  });
 });
 
 // table styling data
-
 let table = new Table({
   chars: { 
     'top': '═' , 
@@ -52,28 +73,12 @@ let table = new Table({
 });
  
 table.push(
-  [chalk.cyan('name'), chalk.cyan('confidence'), chalk.cyan('version'), chalk.cyan('categories'), chalk.cyan('website')], 
+  [
+    chalk.cyan('name'), 
+    chalk.cyan('confidence'), 
+    chalk.cyan('version'), 
+    chalk.cyan('categories'), 
+    chalk.cyan('website')
+  ], 
   ['frob', 'bar', 'quuz', 'fdsf', '232sdd']
 );
-
-// spinner start
-
-const spinner = ora('Loading unicorns').start();
-
-const options = {
-  userAgent: 'Wappalyzer',
-  maxWait: 3000,
-  debug: false
-};
-
-const wappalyzer = require('wappalyzer')(options);
-
-wappalyzer.analyze('http://numbersapi.com')
-  .then(json => {
-    spinner.stop();
-    console.log(JSON.stringify(json, null, 2));
-    console.log(table.toString());
-  })
-  .catch(error => {
-    console.error(error);
-  });
