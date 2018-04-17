e7745279363f4cafc0329bbe2e08fb7dd527d3aa
Daniel Ruf <daniel@daniel-ruf.de>
2018-04-17 23:54:09 +0200

    tests: use the new Wappalyzer API

--- test.js
+++ test.js
@@ -1,14 +1,15 @@
 const should = require('should');
+const Wappalyzer = require('wappalyzer');
 const options = {
   userAgent: 'Wappalyzer',
   maxWait: 8000,
   debug: false
 };
-const wappalyzer = require('wappalyzer')(options);
+const wappalyzer = new Wappalyzer('https://github.com', options);
 
 describe('#wappalyzer', () => {
   it('should return data without null or empty', done => {
-    wappalyzer.analyze('https://github.com')
+    wappalyzer.analyze()
     .then(json => {
       (json.length < 0).should.be.true;
     })
