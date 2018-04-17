79c2d88109ffac0a7499207d3c2787deac4bd181
Splitty <splittydev@protonmail.com>
2018-04-17 15:57:20 +0200

    Fix breaking background-color issue

--- bin/cli.js
+++ bin/cli.js
@@ -178,7 +178,7 @@ let run = function (obj) {
       font: 'block',           // define the font face
       align: 'left',         // define text alignment
       colors: ['cyan'],        // define all colors
-      background: 'Black',     // define the background color
+      background: 'black',     // define the background color
       letterSpacing: 1,        // define letter spacing
       lineHeight: 1,           // define the line height
       space: true,             // define if the output text should have empty lines on top and on the bottom
